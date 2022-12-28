6b29b5126a085b75d9ae8154ca5b4cc4684a7325
Nicholas Miller <nicholasmill157@gmail.com>
2022-12-27 20:08:34 -0500

    cleaned up ignored files and added onto server.js

--- server.js
+++ server.js
@@ -109,6 +109,16 @@ const promptUser = () => {
 };
 
 //Department prompt
+showDepartments = () => {
+  console.log('Showing all departments...\n');
+  const sql = `SELECT department.id AS id, department.name AS department FROM department`; 
+
+  connection.promise().query(sql, (err, rows) => {
+    if (err) throw err;
+    console.table(rows);
+    promptUser();
+  });
+};
 
 //Role prompt
 
